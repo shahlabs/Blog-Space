85d7d7f07af29cc31ed4a9ab0ed254fe3b5e2d15
Paladi <paladirajagopal.p@husky.neu.edu>
2018-02-13 17:46:53 -0500

    database change

--- Angular2/routes/authentication.js
+++ Angular2/routes/authentication.js
@@ -24,37 +24,13 @@ module.exports = (router) => {
             password: req.body.password
           });
           // Save user to database
+          // res.send(user);
+
           user.save((err) => {
-            // Check if error occured
-            if (err) {
-              // Check if error is an error indicating duplicate account
-              // if (err.code === 11000) {
-              //   res.json({ success: false, message: 'Username or e-mail already exists' }); // Return error
-              // } else {
-              //   // Check if error is a validation rror
-              //   if (err.errors) {
-              //     // Check if validation error is in the email field
-              //     if (err.errors.email) {
-              //       res.json({ success: false, message: err.errors.email.message }); // Return error
-              //     } else {
-              //       // Check if validation error is in the username field
-              //       if (err.errors.username) {
-              //         res.json({ success: false, message: err.errors.username.message }); // Return error
-              //       } else {
-              //         // Check if validation error is in the password field
-              //         if (err.errors.password) {
-              //           res.json({ success: false, message: err.errors.password.message }); // Return error
-              //         } else {
-              //           res.json({ success: false, message: err }); // Return any other error not already covered
-              //         }
-              //       }
-              //     }
-              //   } else {
-                  res.json({ success: false, message: 'Could not save user. Error: ', err }); // Return error if not related to validation
-                // }
-              // }
+            if (err)
+              res.json({success: false, message: 'Could not save the user! Error:', err});
             } else {
-              res.json({ success: true, message: 'Account registered!' }); // Return success
+              res.json ({success: true, message: 'Account created successfully!'});
             }
           });
         }
